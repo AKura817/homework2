caf6c4becd7507f67f55dbdaf457956761508600
AKura817 <kuraksinartem817@gmail.com>
2026-05-11 13:52:55 +0300

    Add files via upload

---
+++ logical.js
@@ -0,0 +1,14 @@
+let age = 20;
+let name = "Artem";
+let isStudent = true;
+let salary = 500;
+
+console.log(age > 18);
+console.log(age < 18);
+console.log(age == "20");
+console.log(age === "20");
+console.log(salary >= 300);
+
+console.log(age > 18 && isStudent);
+console.log(age > 25 || isStudent);
+console.log(!isStudent);
